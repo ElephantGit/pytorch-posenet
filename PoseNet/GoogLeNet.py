9f94e8c4fa4abaa3c354b61f0496d394debdeb1a
yc <195216889@qq.com>
2018-06-01 11:09:27 +0800

    complete GoogLeNet

--- PoseNet/GoogLeNet.py
+++ PoseNet/GoogLeNet.py
@@ -1,7 +1,7 @@
 '''GoogLeNet with PyTorch'''
 import torch
 import torch.nn as nn
-
+import torch.nn.functional as F
 from torch.autograd import Variable
 
 
@@ -47,7 +47,7 @@ class InceptionV1(nn.Module):
 
 class GoogLeNet(nn.Module):
     def __int__(self):
-        super(PoseNet, self).__init__()
+        super(GoogLeNet, self).__init__()
 
         self.pre_layers = nn.Sequential(
             nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3),
@@ -76,12 +76,16 @@ class GoogLeNet(nn.Module):
         self.b5 = InceptionV1(832, 384, 192, 384, 48, 128, 128)
 
         self.avg_pool = nn.AvgPool2d(kernel_size=7, stride=1)
-        self.fc = nn.Linear(1024, 2048)
+        self.avg_pool5x5 = nn.AvgPool2d(kernel_size=5, stride=3)
+        self.fc = nn.Linear(1024, 1000)
+        self.fc_128 = nn.Linear(128, 1024)
+        self.soft_max = nn.Softmax2d()
+        self.conv1x1 = nn.Conv2d(512, 128, kernel_size=1, stride=1)
+        self.conv1x1_2 = nn.Conv2d(528, 128, kernel_size=1, stride=1)
 
-        self.dropout = nn.Dropout(p=0.5)
+        self.dropout_4 = nn.Dropout(p=0.4)
+        self.dropout_7 = nn.Dropout(p=0.7)
         self.relu = nn.ReLU()
-        self.cls_fc_pose_xyz = nn.Linear(2048, 3)
-        self.cls_fc_pose_wpqr = nn.Linear(2048, 4)
 
     def forward(self, x):
         out = self.pre_layers(x)
@@ -92,48 +96,53 @@ class GoogLeNet(nn.Module):
         out = self.max_pool(out)
 
         out = self.a4(out)
-        cls1_pool = self.avg_pool(out)
-        cls1_fc1 = self.fc(cls1_pool)
+
+        # 5x5 pooling
+        cls1_pool = self.avg_pool5x5(out)
+        # 1x1 conv
+        cls1_reduction = self.conv1x1(cls1_pool)
+        cls1_reduction = self.relu(cls1_reduction)
+        cls1_reduction = cls1_reduction.view(cls1_reduction.size(0), -1)
+        # 128 fc
+        cls1_fc1 = self.fc_128(cls1_reduction)
         cls1_fc1 = self.relu(cls1_fc1)
-        cls1_fc1 = self.dropout(cls1_fc1)
-        cls1_fc_pose_xyz = self.cls_fc_pose_xyz(cls1_fc1)
-        cls1_pose_wpqr = self.cls_fc_pose_wpqr(cls1_fc1)
+        # 0.7 dropout
+        cls1_fc1 = self.dropout_7(cls1_fc1)
+        # 1024 fc
+        cls1_fc2 = self.fc(cls1_fc1)
+        # softmax
+        cls1 = self.soft_max(cls1_fc2)
+
         out = self.b4(out)
         out = self.c4(out)
         out = self.d4(out)
-        cls2_pool = self.avg_pool(out)
-        cls2_fc1 = self.fc(cls2_pool)
+
+        # 5x5 pooling
+        cls2_pool = self.avg_pool5x5(out)
+        # 1x1 conv
+        cls2_reduction = self.conv1x1_2(cls2_pool)
+        cls2_reduction = self.relu(cls2_reduction)
+        cls2_reduction = cls2_reduction.view(cls2_reduction.size(0), -1)
+        # 128 fc
+        cls2_fc1 = self.fc_128(cls2_reduction)
         cls2_fc1 = self.relu(cls2_fc1)
-        cls2_fc1 = self.dropout(cls2_fc1)
-        cls2_fc_pose_xyz = self.cls_fc_pose_xyz(cls2_fc1)
-        cls2_pose_wpqr = self.cls_fc_pose_wpqr(cls2_fc1)
+        # 0.7 dropout
+        cls2_fc1 = self.dropout_7(cls2_fc1)
+        # 1024 fc
+        cls2_fc2 = self.fc(cls2_fc1)
+        # softmax
+        cls2 = self.soft_max(cls2_fc2)
+
         out = self.e4(out)
 
         out = self.max_pool(out)
 
         out = self.a5(out)
         out = self.b5(out)
-        out = self.avg_pool(out)
-        out = out.view(out.size(0), -1)
         cls3_pool = self.avg_pool(out)
-        cls3_fc1 = self.fc(cls3_pool)
-        cls3_fc1 = self.relu(cls3_fc1)
-        cls3_fc1 = self.dropout(cls3_fc1)
-        cls3_fc_pose_xyz = self.cls_fc_pose_xyz(cls3_fc1)
-        cls3_pose_wpqr = self.cls_fc_pose_wpqr(cls3_fc1)
+        cls3_fc = self.fc(cls3_pool)
+        cls3_fc = self.relu(cls3_fc)
+        cls3_fc = self.dropout_4(cls3_fc)
+        cls3 = self.soft_max(cls3_fc)
 
-        out = [
-            [cls1_fc_pose_xyz, cls1_pose_wpqr],
-            [cls2_fc_pose_xyz, cls2_pose_wpqr],
-            [cls3_fc_pose_xyz, cls3_pose_wpqr]
-        ]
-        return out
-
-
-net = GoogLeNet().cuda()
-x = torch.randn(1, 3, 32, 32)
-print(x)
-print(x.size())
-y = net(Variable(x).cuda())
-
-print(y.size())
+        return cls1, cls2, cls3
